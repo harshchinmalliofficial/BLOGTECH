0c4bca075861a8f821122c382eec2b6ee37cec43
HarshChinmalli <harshchinmalliofficial@gmail.com>
2024-11-08 16:36:42 +0530

    mess2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["mongoose"], // Correct key
+    serverExternalPackages: ["mongoose"], // Updated key
   },
   images: {
     domains: ["lh3.googleusercontent.com"], // Allowed image domains
